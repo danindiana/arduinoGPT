5c9155e4b01f13aa6b3321e3d8ad4dbdab46bbb8
danindiana <danindiana@users.noreply.github.com>
2023-05-08 00:38:28 -0500

    Create laser_offset_PIDRTtune.ino
    Ref: https://github.com/br3ttb/Arduino-PID-Library/tree/master
    Ref: https://github.com/ElectronicCats/mpu6050
    See patent: US8766152B1
    
    Refactored the previous sketch and added the ability to change PID tuning parameters while the system is running. The sketch now accepts serial commands to adjust the PID tuning parameters (kP, kI, and kD) and prints the current values upon request. The accepted commands are:
    
    P:value - set the proportional gain (kP) to value
    I:value - set the integral gain (kI) to value
    D:value - set the derivative gain (kD) to value
    ? - print the current PID tuning parameters
    Here's the complete refactored Arduino sketch:

---
+++ laser_offset_PIDRTtune.ino
@@ -0,0 +1,98 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+#include <Servo.h>
+#include <Encoder.h>
+#include <MPU6050_tockn.h>
+#include <PID_v1.h>
+
+// Constants
+const int opticalSensorPin = 2;
+const int servoXPin = 9;
+const int servoYPin = 10;
+const int threshold = 100;
+
+// Encoder pins for X and Y axes
+const int encoderXPins[] = {3, 4};
+const int encoderYPins[] = {5, 6};
+
+// Variables
+int opticalSensorValue;
+int servoXPosition = 90;
+int servoYPosition = 90;
+
+// Target position and control gain
+float targetX = 0;
+float targetY = 0;
+
+// Servo objects
+Servo servoX;
+Servo servoY;
+
+// BMP280 object
+Adafruit_BMP280 bmp280;
+
+// Encoders for X and Y axes
+Encoder encoderX(encoderXPins[0], encoderXPins[1]);
+Encoder encoderY(encoderYPins[0], encoderYPins[1]);
+
+// MPU6050 object
+MPU6050 mpu6050(Wire);
+
+// PID Controllers
+double inputX, inputY, outputX, outputY;
+double setpointX = 0, setpointY = 0;
+double kP = 1, kI = 1, kD = 1;
+
+PID pidX(&inputX, &outputX, &setpointX, kP, kI, kD, DIRECT);
+PID pidY(&inputY, &outputY, &setpointY, kP, kI, kD, DIRECT);
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!bmp280.begin()) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+
+  pinMode(opticalSensorPin, INPUT);
+
+  servoX.attach(servoXPin);
+  servoY.attach(servoYPin);
+
+  servoX.write(servoXPosition);
+  servoY.write(servoYPosition);
+
+  // Initialize MPU6050
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+
+  // Initialize PID controllers
+  pidX.SetMode(AUTOMATIC);
+  pidY.SetMode(AUTOMATIC);
+}
+
+void loop() {
+  opticalSensorValue = digitalRead(opticalSensorPin);
+  float currentHeight = getCurrentHeight();
+
+  // If the canister is aligned with the laser, adjust the control system
+  if (opticalSensorValue == HIGH) {
+    alignWithLaser();
+  }
+
+  // If the canister has traversed the predetermined point in the descending trajectory
+  if (currentHeight < threshold) {
+    moveToOffsetIndicator();
+  }
+
+  // Check for and process serial commands to adjust PID tuning parameters
+  if (Serial.available()) {
+    processSerialCommand();
+  }
+
+  delay(50);
+}
+
+void alignWithL
