f79d3ddb18e11a0626ec8300a119d340c101b5fa
danindiana <danindiana@users.noreply.github.com>
2023-05-07 23:58:01 -0500

    Update laser_offset_BMP280.ino
    Combine with laser_impact_offset.ino
    
    To implement the getCurrentHeight() function, you can use a barometric pressure sensor like the BMP280 to measure the altitude. Here's an example implementation using the BMP280 sensor and the Adafruit BMP280 library:
    
    First, install the Adafruit BMP280 library using the Arduino Library Manager. To do this, open the Arduino IDE, click on Sketch > Include Library > Manage Libraries, then search for "Adafruit BMP280" and install it.
    
    Next, include the necessary libraries and initialize the BMP280 sensor:
    
    Now, you can implement the getCurrentHeight() function, which reads the altitude from the BMP280 sensor and returns it as a float:
    
    Make sure to adjust the altitude readings for your specific use case and conditions. The readAltitude() function returns the altitude in meters, relative to the sea level. The sea level pressure might vary depending on your location and the weather conditions, so you may need to update it accordingly using the bmp280.setSeaLevelPressure() function.
    
    Here's the complete example with the BMP280 sensor integrated:

--- laser_offset_BMP280.ino
+++ laser_offset_BMP280.ino
@@ -1,7 +1,24 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
+#include <Servo.h>
 
+// Constants
+const int opticalSensorPin = 2;
+const int servoXPin = 9;
+const int servoYPin = 10;
+const int threshold = 100;
+
+// Variables
+int opticalSensorValue;
+int servoXPosition = 90;
+int servoYPosition = 90;
+
+// Servo objects
+Servo servoX;
+Servo servoY;
+
+// BMP280 object
 Adafruit_BMP280 bmp280;
 
 void setup() {
@@ -11,10 +28,48 @@ void setup() {
     Serial.println("Could not find a valid BMP280 sensor, check wiring!");
     while (1);
   }
-  
-  // ... rest of your setup code
+
+  pinMode(opticalSensorPin, INPUT);
+
+  servoX.attach(servoXPin);
+  servoY.attach(servoYPin);
+
+  servoX.write(servoXPosition);
+  servoY.write(servoYPosition);
 }
+
+void loop() {
+  opticalSensorValue = digitalRead(opticalSensorPin);
+  float currentHeight = getCurrentHeight();
+
+  // If the canister is aligned with the laser, adjust the control system
+  if (opticalSensorValue == HIGH) {
+    alignWithLaser();
+  }
+
+  // If the canister has traversed the predetermined point in the descending trajectory
+  if (currentHeight < threshold) {
+    moveToOffsetIndicator();
+  }
+
+  delay(50);
+}
+
+void alignWithLaser() {
+  // Implement control logic here to align the canister with the laser
+  
+  // As an example, we adjust the servo positions by a small increment
+  servoXPosition = (servoXPosition + 1) % 180;
+  servoYPosition = (servoYPosition + 1) % 180;
+
+  servoX.write(servoXPosition);
+  servoY.write(servoYPosition);
+}
+
 float getCurrentHeight() {
   float altitude = bmp280.readAltitude();
   return altitude;
 }
+
+void moveToOffsetIndicator() {
+  // Implement logic to guide the can
